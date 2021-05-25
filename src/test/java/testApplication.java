3895530074d7096b1108bba3037b13003cd6220d
punitha <pdpunitha@gmail.com>
2021-05-25 22:33:23 +0100

    this is commmit

---
+++ src/test/java/testApplication.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+public class testApplication {
+    @Test
+    public void m1(){
+        System.out.println("method1 calling");
+    }
+    @Test
+    public void m2(){
+        System.out.println("method2 calling");
+    }
+    @Test
+    public void m3(){
+        System.out.println("method3 calling");
+    }
+}
